d598aeabc51fdc743a7d77d76313922b1d7d7cae
V S <vrishali0115@gmail.com>
2019-11-01 06:30:59 +0530

    hibernate: committing in-memory helloworld

---
+++ src/main/java/com/hibernate/a/inmemory/helloworld/HibernateUtil.java
@@ -0,0 +1,30 @@
+package com.hibernate.a.inmemory.helloworld;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration configuration = (new Configuration()).configure("/hibernate.inmemory.cfg.xml");
+			StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+			serviceRegistryBuilder.applySettings(configuration.getProperties());
+			org.hibernate.service.ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+		return sessionFactory;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+
+}
---
+++ src/main/java/com/hibernate/a/inmemory/helloworld/TestInMemory.java
@@ -0,0 +1,24 @@
+package com.hibernate.a.inmemory.helloworld;
+
+import org.hibernate.Session;
+
+public class TestInMemory {
+
+	public static void main(String args[]) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession();) {
+			createStudent(session);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		} finally {
+			HibernateUtil.shutdown();
+		}
+	}
+
+	private static void createStudent(Session session) {
+		Student student = new Student();
+		student.setId(1);
+		student.setName("Vrishali");
+		session.save(student);
+	}
+
+}
